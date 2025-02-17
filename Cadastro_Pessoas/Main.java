c55e0b521425080274c6c4d7304d4cb5e065f6b5
09Ghost90 <brunosousapereira.az@gmail.com>
2025-02-16 21:37:10 -0300

    Coleta via terminal

--- Cadastro_Pessoas/Main.java
+++ Cadastro_Pessoas/Main.java
@@ -1,8 +1,29 @@
 package Cadastro_Pessoas;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
         Pessoa p = new Pessoa("Bruno", 19, "Computação");
+
+        // Coletar entrada
+        System.out.println("Digite o nome: ");
+        String nome = scanner.next();
+        p.setNome(nome);
+
+        // Coletando a idade
+        System.out.println("Digite a idade: ");
+        int idade = scanner.nextInt();
+        p.setIdade(idade);
+
+        // Coletando o curso
+        System.out.println("Digite o curso: ");
+        String curso = scanner.next();
+        p.setCurso(curso);
+
+        scanner.close();
 
         System.out.println("Nome: " + p.getNome());
         System.out.println("Idade: " + p.getIdade());
